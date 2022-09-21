04338a5f1fa22c5d7b1f4b844c1e97c671ba9bdf
ore291 <oreoluwapadonu@gmail.com>
2022-09-21 19:33:12 +0100

    .

--- app/Http/Middleware/EnsurePremiumUser.php
+++ app/Http/Middleware/EnsurePremiumUser.php
@@ -26,7 +26,13 @@ class EnsurePremiumUser
         $userSub = Subscription::where('user_id', $id)->whereIn('plan_id', [1, 2, 3, 4])->first();
         $today = Carbon::today();
         $plans = Stats::whereStrict('name', 'plans')->first();
-        $plans_array = explode(',', $plans->value);
+
+        $plans_array = array(1);
+
+        if(isset($plans)){
+               $plans_array = explode(',', $plans->value);
+        }
+     
 
         if (in_array($userSub->plan_id, $plans_array)  && $today->between($userSub->start_date, $userSub->end_date)) {
             return $next($request);
